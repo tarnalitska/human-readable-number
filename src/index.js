25682b1c50bc310758f25f7527ae47f629ac3b80
Sofya Tarnalitskaya <tarnalitska@gmail.com>
2021-03-11 21:40:31 +0300

    feat: solution for human readable number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function toReadable (number) {
-  
+    let units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'];
+    let tens = ['','', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (number == 0) {
+        return 'zero';
+    }
+
+    if (number <= 20) {
+        return units[number];
+    }
+
+    if (number.toString().length == 2 && number % 10 == 0) {
+        return tens[parseInt((''+number)[0])];
+    }
+
+    if (number.toString().length == 2) {
+        return tens[parseInt((''+number)[0])] + ' ' + units[parseInt((''+number)[1])];
+    }
+
+    if (number.toString().length == 3 && number % 100 == 0){
+        return units[parseInt((''+number)[0])] + ' hundred';
+    }
+
+    if (number.toString().length == 3 && parseInt(number.toString().substr(1)) <= 20 ) {
+        let x = parseInt(number.toString().substr(1));
+        return units[parseInt((''+number)[0])] + ' hundred ' + units[x];
+    }
+
+    if (number.toString().length == 3 && number % 10 == 0) {
+        return units[parseInt((''+number)[0])] + ' hundred ' + tens[parseInt((''+number)[1])];
+    }
+
+    if (number.toString().length == 3) {
+        return units[parseInt((''+number)[0])] + ' hundred ' + tens[parseInt((''+number)[1])] + ' ' + units[parseInt((''+number)[2])];
+    }
+
 }
